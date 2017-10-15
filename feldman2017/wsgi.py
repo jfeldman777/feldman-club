0e5036849073bf0120ca56d616c72d6dc4b4360c
jfeldman777 <jfeldman777@gmail.com>
2017-10-15 22:20:27 +0300

    wsgi whitenoise

--- feldman2017/wsgi.py
+++ feldman2017/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "feldman2017.settings")
 
 application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
